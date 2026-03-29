a77941bc913fbe166683b9055e98560c5cca9d41
adityaxsundar <aditya2006sundar@gmail.com>
2026-03-29 18:43:52 +0530

    User Profile added

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -7,13 +7,19 @@ const {
   getAvailableProducts,
   startAuction,
   getPendingProducts,
-  approveProduct
+  approveProduct,
+  getMyProducts,
+  deleteProduct,
+  editProduct
 } = require('../controllers/product.controller');
 
 // PUBLIC / USER ROUTES
 router.post('/add', verifyToken, upload.single('productPic'), createProduct);
 router.get('/', getAvailableProducts);
 router.post('/start-auction', verifyToken, startAuction);
+router.get('/my-products', verifyToken, getMyProducts);
+router.delete('/:id', verifyToken, deleteProduct);
+router.put('/:id', verifyToken, upload.single('productPic'), editProduct);
 
 // ADMIN SPECIFIC ROUTES
 router.get('/admin/pending', verifyToken, isAdmin, getPendingProducts);
